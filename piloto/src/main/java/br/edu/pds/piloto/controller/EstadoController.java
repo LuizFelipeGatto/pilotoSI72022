6b2d78bf02c227b9e1257b6d65d6f2e89a9d2009
LuizFelipeGatto <luizfelipegatto08@gmail.com>
2022-04-07 20:05:00 -0300

    Subindo partes do crdu

--- piloto/src/main/java/br/edu/pds/piloto/controller/EstadoController.java
+++ piloto/src/main/java/br/edu/pds/piloto/controller/EstadoController.java
@@ -4,8 +4,12 @@ import br.edu.pds.piloto.model.Estado;
 import br.edu.pds.piloto.repository.EstadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class EstadoController {
@@ -18,6 +22,25 @@ public class EstadoController {
         ModelAndView mv = new ModelAndView("estado");
         mv.addObject("estado", estado);
         return mv;
+    }
+
+    @GetMapping("/listarEstado")
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView("estados");
+        mv.addObject("estados", estadoRepositorio.findAll());
+        return mv;
+    }
+
+
+    @PostMapping("/salvarEstado")
+    public ModelAndView salvar(@Valid Estado estado, BindingResult result){
+        if(result.hasErrors()){
+            return cadastrar(estado);
+        }
+
+        estadoRepositorio.saveAndFlush(estado);
+
+        return listar();
     }
 
 }
